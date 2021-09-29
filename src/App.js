ff3e84ebf2f11ec94b887e5bca58931bfa7e5b6c
Mohammad Mostafa <mohammad-mostafa94>
2021-09-29 20:43:00 +0600

    user added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+    // const [users, setUsers] = useState([]);
+
+    // useEffect(() => {
+    //   fetch("https://jsonplaceholder.typicode.com/users")
+    //   .then(res => res.json())
+    //   .then(data => setUsers(data))
+    // }, []);
+    const [count, setCount] = useState(100);
+    const counter = () => {
+      
+      if (count > 0) {
+        setCount(count - 10);
+        return;
+      }
+    }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>count:{count}</h1>
+    <button onClick={counter}>Decrease</button>
+    {/* <h1>{users.length}</h1>
+    {
+      users.map(user => <User name = {user.name} email = {user.email}></User>)
+    }
+      <User></User> */}
     </div>
   );
+}
+const User = (props)=>{
+  return (
+    <div>
+      <h1>Name:{props.name}</h1>
+      <p>email:{props.email}</p>
+    </div>
+  )
 }
 
 export default App;
